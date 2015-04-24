ab52b7c231724ef378a7005a68dcf9f001337eca
Dennis Wilson <d9w@mit.edu>
2015-04-25 01:42:34 +0300

    python constraint check

--- Python/WindFarmLayoutEvaluator.py
+++ Python/WindFarmLayoutEvaluator.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy
 import xml.etree.ElementTree as ET
 
 class WindScenario:
@@ -27,9 +28,39 @@ class WindScenario:
         self.cs = numpy.array(scenario['c'])
         self.ks = numpy.array(scenario['k'])
         self.omegas = numpy.array(scenario['omega'])
-        self.thetas = numpy.array(scenario['theta'])
+        thetas = numpy.array(scenario['theta'])
+        self.thetas = numpy.asmatrix(numpy.vstack((thetas, thetas+15)).T)
         self.obstacles = scenario['obstacle']
         self.width = scenario['Width']
         self.height = scenario['Height']
         self.nturbines = scenario['NTurbines']
         self.energy = scenario['WakeFreeEnergy']
+        self.CT=0.8;
+        self.PRated=1500.0;
+        self.R=38.5;
+        self.eta=-500.0;
+        self.k=0.0750;
+        self.slambda=140.86;
+        self.vCin=3.5;
+        self.vCout=20;
+        self.vRated=14;
+
+class WindFarmLayoutEvaluator:
+
+    def __init__(self, ws):
+        self.ws = ws
+
+    def evaluate(self, scenario):
+        pass
+
+    def check_constraint(self, layout):
+        dists = scipy.spatial.distance.pdist(layout)
+        if any(dists <= 8*self.ws.R):
+            return False
+        for obs in self.ws.obstacles:
+            bpositions = numpy.multiply(
+                    layout <= [obs['xmax'], obs['ymax']],
+                    layout >= [obs['xmin'], obs['ymin']])
+            if any(numpy.multiply(bpositions[:,0], bpositions[:,1])):
+                return False
+        return True
