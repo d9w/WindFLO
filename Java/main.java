7ebd02739d41ca13cb40ad4563e85f0665bcd7f5
Sylvain <cussat@dhcp-54-134.univ-tlse1.fr>
2015-06-16 10:08:56 +0200

    Bug in the Java version: scenarios ere going from 4 to 4 instead of 0 to 4 in the main GA file

--- Java/main.java
+++ Java/main.java
@@ -3,8 +3,9 @@ public class main {
   public static void main(String argv[]) {
       try {
 	  String userToken = new String("USER TOKEN GOES HERE");
-	  String runToken;
-	  for (int sc = 4; sc < 5; sc++) {
+	  String runToken = null;
+	  System.out.println("Your user token is: "+userToken);
+	  for (int sc = 0; sc < 5; sc++) {
 	      // Create the competition evaluator
 	      CompetitionEvaluator eval = new CompetitionEvaluator();
 	      // initialize the evalutor with the scenario id (from 0 to 4), with you user token (see you account online) and the run token. For the first run, ommit the run token, the server will send you (and display) a new one. This run token can be recovered with the getRunToken() method from CompetitionEvaluator
@@ -15,6 +16,7 @@ public class main {
 		  eval.initialize(sc, userToken, runToken);
 	      }
 	      GA algorithm = new GA(eval);
+	      System.out.println("The current run token is: "+runToken);
 	      algorithm.run(); // optional, name of method 'run' provided on submission
 	      // algorithm can also just use constructor
 	  }
