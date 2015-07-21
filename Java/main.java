25304dc48f39b5d72cbe876874c750a9b064d861
Sylvain <cussat@dhcp-54-145.univ-tlse1.fr>
2015-07-21 09:10:36 +0200

    Past results

--- Java/main.java
+++ Java/main.java
@@ -1,6 +1,6 @@
 public class main {
 
-  public static void main(String argv[]) {
+  public static void main(String argv[]) throws Exception {
       try {
 	  String userToken = new String("USER TOKEN GOES HERE");
 	  String runToken = null;
